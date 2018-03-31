4d7b43bdc0d89a87758e7a74513d9dcbebb984ec
powerfuldeff <powerfuldeff@gmail.com>
2018-03-31 18:13:51 +0300

    Added functions

--- main.py
+++ main.py
@@ -2,8 +2,7 @@
 
 import sys
 import time
-import warnings
-import drawing as dr
+import drawing
 import bernoulli
 
 # Constants
@@ -12,12 +11,15 @@ accuracy = 0.1 ** 5
 
 # Coefficients of a polynomial
 # Коэффициенты полинома
-coefficients = [1, -15, 85, -225, 274, -120]
+coefficients0 = [1, -15, 85, -225, 274, -120]
+coefficients1 = [1, -10, 35, -50, 24]
+coefficients2 = [1, -6, 11, -6]
+coefficients3 = [1, -3, 2]
 
 
 # The polynomial at the point
 # Полином в точке
-def f(point, a=coefficients):
+def f(point, a=coefficients0):
     result = 0
     power = len(a)
 
@@ -28,17 +30,34 @@ def f(point, a=coefficients):
 
 
 if __name__ == "__main__":
-    dr.build_function(f,
-                      coefficients,
-                      "Первая функция системы",
-                      -1, 6,
-                      -4, 4
-                      )
+    sys.setrecursionlimit(500)
 
-    dr.show_plot()
+    # Drawing
+    # Построение
+    drawing.build_function(f, coefficients0, "f5(x)", -1, 6, -4, 4)
+    drawing.build_function(f, coefficients1, "f4(x)", -1, 6, -4, 4)
+    drawing.build_function(f, coefficients2, "f3(x)", -1, 6, -4, 4)
+    drawing.build_function(f, coefficients3, "f2(x)", -1, 6, -4, 4)
+
+    drawing.show_plot()
 
     start = time.time()
-    print(bernoulli.execute_method_interface(f, coefficients, [1, 1, 2, 1, 2], 0.1**5))
+    print(bernoulli.execute_method_interface(f, coefficients0, [5, 4, 3, 2, 1], 0.1 ** 5))
+    end = time.time()
+    print('{:f}'.format(end - start))
+
+    start = time.time()
+    print(bernoulli.execute_method_interface(f, coefficients1, [5, 4, 3, 2], 0.1 ** 5))
+    end = time.time()
+    print('{:f}'.format(end - start))
+
+    start = time.time()
+    print(bernoulli.execute_method_interface(f, coefficients2, [5, 4, 3], 0.1 ** 5))
+    end = time.time()
+    print('{:f}'.format(end - start))
+
+    start = time.time()
+    print(bernoulli.execute_method_interface(f, coefficients3, [5, 4], 0.1 ** 5))
     end = time.time()
     print('{:f}'.format(end - start))
 
