9e4ae6568564bd3481246d1abf1784ccf52cecb3
powerfuldeff <powerfuldeff@gmail.com>
2018-04-02 11:35:55 +0300

    Lobachevsky working

--- lobachevsky.py
+++ lobachevsky.py
@@ -1,9 +1,9 @@
 def lobachevsky_method(f, coefficients, iterations, accuracy=0.1 ** 5):
     if iterations > 0:
 
-        temp_x1 = (coefficients[1] / coefficients[0]) ** (1 / 2 ** iterations)
-        temp_x2 = (coefficients[2] / coefficients[1]) ** (1 / 2 ** iterations)
-        temp_x3 = (coefficients[3] / coefficients[2]) ** (1 / 2 ** iterations)
+        temp_x1 = (coefficients[1] / coefficients[0]) ** (1 / 2 ** (iterations - 1))
+        temp_x2 = (coefficients[2] / coefficients[1]) ** (1 / 2 ** (iterations - 1))
+        temp_x3 = (coefficients[3] / coefficients[2]) ** (1 / 2 ** (iterations - 1))
 
         if (abs(f(temp_x1, coefficients)) <= accuracy or (f(-temp_x1, coefficients)) <= accuracy) and (
                 abs(f(temp_x2, coefficients)) <= accuracy or (f(-temp_x2, coefficients)) <= accuracy) and (
